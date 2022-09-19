0047ad3150c3f5750dd528b2663d83ceb55e09ed
AbshenovS <abshenovs.21@gmail.com>
2022-09-19 14:10:35 +0600

    Configure Github actions

---
+++ .github/scripts/copy_to_s3.sh
@@ -0,0 +1,17 @@
+#!/bin/sh 
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+mkdir -p ~/.aws
+
+touch ~/.aws/credentials
+
+echo "[default]
+aws_access_key_id = ${AWS_ACCESS_KEY_ID}
+aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
+
+aws s3 sync ${FOLDER} s3://${S3_BUCKET}/${S3_KEY} --region ${AWS_REGION} 
+
+rm -rf ~/.aws/credentials
